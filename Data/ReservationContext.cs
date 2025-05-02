da86d06b5812239b3691b00ad2c0db2275ec6f24
muratsaavas <murat.savs1@gmail.com>
2025-05-02 04:15:34 +0300

    C:\Users\Murat\Documents\GitHub\Indoor-Football-Reservation-System\20210305022_IndoorFootballReservation\

---
+++ Data/ReservationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IndoorFootballReservation.Data
+{
+    public class ReservationContext : DbContext
+    {
+        public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;database=reservation_db;user=root;password=;",
+                new MySqlServerVersion(new Version(8, 0, 36))
+            );
+        }
+    }
+}
